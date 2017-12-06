f65cc6148ec45d046928299bbb59d0d7b122bab2
coustel <kevin.coustel@laposte.net>
2017-12-06 17:12:13 +0100

    livredor

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,7 +5,8 @@ use Silex\Application;
 //cette ligne nous permet d'utiliser le service fourni par symfony pour gérer 
 // les $_GET et $_POST
 use Symfony\Component\HttpFoundation\Request;
-
+use WF3\Domain\Livredor;
+use WF3\Form\Type\LivredorType;
 
 class HomeController{
 
@@ -25,8 +26,18 @@ class HomeController{
 	}
 	
 	
-	//page d'accueil du back office
-	public function livreDorAction(Application $app){
-		return $app['twig']->render('livredor.html.twig');
+	//page du livre d'or
+	public function livreDorAction(Application $app, Request $request){
+		$livredor = new Livredor();
+		$livredorForm = $app['form.factory']->create(LivredorType::class, $livredor);
+		$livredorForm->handleRequest($request);
+		if($livredorForm->isSubmitted() && $livredorForm->isValid()){
+			$app['dao.livredor']->insert($livredor);
+		}
+
+
+		return $app['twig']->render('livredor.html.twig',
+			array('livredorForm'=>$livredorForm->createView())
+		);
 	}
 }

bf11b9a2d860a340da45aeb4786eb6b9b5379e9e
Nauno40 <batty.arnaud@hotmail.fr>
2017-12-13 11:24:21 +0100

    sd
    sd

--- src/Form/Type/ReservationType.php
+++ src/Form/Type/ReservationType.php
@@ -8,7 +8,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Validator\Constraints as Assert;
 
 
